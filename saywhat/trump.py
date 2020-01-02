88badbeff14d21bd4e5cc85f428152108070ef5a
Kyle <kylepw@gmail.com>
2020-01-02 13:28:35 +0900

    Get rate limit status

--- saywhat/trump.py
+++ saywhat/trump.py
@@ -1,5 +1,15 @@
+import json
 import os
 import tweepy
+
+
+def get_rl_status(api):
+    status = api.rate_limit_status()
+    print(
+        'rate_limit_status:',
+        status['resources']['application']['/application/rate_limit_status'],
+    )
+    print('user_timeline:', status['resources']['statuses']['/statuses/user_timeline'])
 
 
 def main():
@@ -9,16 +19,14 @@ def main():
     auth = tweepy.AppAuthHandler(API_KEY, API_SECRET_KEY)
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-    username = 'realDonaldTrump'
-    #id = api.get_user(username).id
+    # get_rl_status()
+
+    sname = 'realDonaldTrump'
     keyword = 'unfair'
 
-    tweets = api.search(keyword+f' from:{username}')
-
-    for t in tweets:
-        print(t.text)
-
-
+    """ for t in tweepy.Cursor(api.user_timeline, tweet_mode='extended', screen_name=sname).items(100):
+        print(t.full_text)
+ """
 
 
 if __name__ == '__main__':
